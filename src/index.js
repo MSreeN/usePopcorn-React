a148f0734eea57778900fb48b32fec238901fa3d
MSreeN <khyathimnsree369@gmail.com>
2024-02-07 19:47:02 +0530

    Click functionality on star

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating="9" />
-    <StarRating />
+    <StarRating maxRating="10" />
     {/* <App /> */}
   </React.StrictMode>
 );
